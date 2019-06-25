0730a7064074af04c953147763553a443c6d4c37
ngoducanh179 <ngoducanh179@gmail.com>
2019-06-25 16:27:01 +0700

    fnejnf

--- index.php
+++ index.php
@@ -82,17 +82,15 @@ window.onclick = function(event) {
 var UserName = document.getElementById('UserName1').value;
 var Password = document.getElementById('PassWord1').value;
 function login1(){
-// if (UserName && Password == "admin"){
-// 	window.location.href = 'ConnectToDB.php';
-// }
-// else if (UserName && Password == "employee"){
-// 	window.location.href = 'InsertData.php';
-// }
-// else {
-// 	return false;
-// }
-
-window.location.href = "https://www.google.com";
+if (UserName && Password == "admin"){
+	window.location.href = 'ConnectToDB.php';
+}
+else if (UserName && Password == "employee"){
+	window.location.href = 'InsertData.php';
+}
+else {
+	return false;
+}
 }
 </script>
 </body>
